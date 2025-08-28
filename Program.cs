3a4fe2e6dc4cdd5850c6bfa1b883d4816daa4b65
Sapana Gartaula <117616753+SapanaGartaula@users.noreply.github.com>
2025-08-28 16:00:14 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using MyFamilyRoot.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container    
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IFamily, Family>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Family}/{action=Familydash}/{id?}");
+
+app.Run();
