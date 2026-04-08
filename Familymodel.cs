5892274ed78d5b3bc843b0146167c17630ea9275
Sapana Gartaula <117616753+SapanaGartaula@users.noreply.github.com>
2026-04-08 10:51:03 +0545

    Add files via upload

---
+++ Familymodel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFamilyRoot.Models
+{
+    public class Familymodel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Description { get; set; }
+    }
+
+}
